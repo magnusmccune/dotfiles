0e10dc78c31771b2afb6809ff57238a1b6d3b268
Magnus McCune <magnusmccune@outlook.com>
2023-07-23 21:29:55 -0300

    Updated PowerShell Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,23 @@ Import-Module -Name Terminal-Icons
 Import-Module -Name PSReadLine
 
 $env:AZ_ENABLED = $true
+
+# Source Control
+function g { Set-Location c:/DATA/repos }
+function gcom
+{
+	git add .
+	git commit -m "$args"`
+}
+function lazyg
+{
+	git add .
+	git commit -m "$args"
+	git push
+}
+
+# Utilities
+
+Function Get-PubIP {
+ (Invoke-WebRequest http://ifconfig.me/ip ).Content
+}
